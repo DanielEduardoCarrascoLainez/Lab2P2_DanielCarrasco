24cdeef5e0b5207f74e9f8b7bb86e1a226491473
DanielEduardoCarrascoLainez <dcarrasco04@unitec.edu>
2023-01-27 13:46:32 -0600

    1-. creacion de proyect y las clases que ocuparemos

---
+++ src/lab2p2_danielcarrasco/Lab2P2_DanielCarrasco.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2p2_danielcarrasco;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Lab2P2_DanielCarrasco {
+    
+    static Scanner ninetales = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //De una solo pongo lo del uss
+        
+        
+    }
+    
+}
