df66cddd36ce2d154aa1e84b8bec9e0fa36ebd71
DanielEduardoCarrascoLainez <dcarrasco04@unitec.edu>
2023-01-27 14:31:50 -0600

    4-. clase edificios

--- src/lab2p2_danielcarrasco/Edificios.java
+++ src/lab2p2_danielcarrasco/Edificios.java
@@ -5,5 +5,42 @@
 package lab2p2_danielcarrasco;
 
 public class Edificios {
+    int numPisos;
+    int locales;
+    String referencia;
+
+    public Edificios() {
+    }
+
+    public Edificios(int numPisos, int locales, String referencia) {
+        this.numPisos = numPisos;
+        this.locales = locales;
+        this.referencia = referencia;
+    }
+
+    public int getNumPisos() {
+        return numPisos;
+    }
+
+    public void setNumPisos(int numPisos) {
+        this.numPisos = numPisos;
+    }
+
+    public int getLocales() {
+        return locales;
+    }
+
+    public void setLocales(int locales) {
+        this.locales = locales;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+    
     
 }
